de3a3df72a2e421534035c6a8a387b440560f0ce
= <=>
2025-01-19 08:29:35 +0900

    added seam and C compatible interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
-use std::error;
+use std::{
+    ffi::{c_char, CStr}, thread::sleep, time::Duration
+};
 
-pub fn handle_remote_http_session(endpoint: String) -> Result<(), Box<dyn error::Error>> {
-    println!("remote session handled using http to endpoint: {}", endpoint);
-    return Ok(())
+#[no_mangle]
+pub unsafe extern "C" fn handle_remote_http_session_rs(endpoint: *const c_char) -> i64 {
+    // this is the unsafe seam, whereby all intermediary processing occurs.
+    let c_str = CStr::from_ptr(endpoint);
+    let result = c_str.to_str();
+    if result.is_err() {
+        return -1;
+    }
+
+    let r_endpoint = result.unwrap();
+    return handle_remote_http_session(r_endpoint);
+}
+
+// this code is all safe now
+fn handle_remote_http_session(endpoint: &str) -> i64 {
+    for i in 1..5 {
+        println!("calling endpoint ({}): {}", i, endpoint);
+        sleep(Duration::from_secs(1));
+    }
+
+    return 0;
 }
